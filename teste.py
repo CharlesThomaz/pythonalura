f97355a75bdd1ef639b3589df29596056263756e
CharlesThomaz <charlesthomaz87@gmail.com>
2023-10-18 12:28:09 -0300

    1

--- teste.py
+++ teste.py
@@ -1,7 +1,19 @@
+"""
+
 for numero in range(1, 20):
     print('O número é {:^80f}'.format(numero))
 
 
+frutas = ["maçã", "banana", "laranja","Limão", "caju", 55, 78, 100]
 
+for indice, fruta in enumerate(frutas):
+    print(f"Índice {indice}: {fruta}")
+
+"""
+
+
+import random
+num = random.random()
+print(f'O  valor é {num}')
 
 
