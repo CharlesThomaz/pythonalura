b7bf34e94e86345d2c9e70d20fc2ab8192fb4304
CharlesThomaz <charlesthomaz87@gmail.com>
2023-11-08 17:01:19 -0300

    08-11-2023

--- cev/A.py
+++ cev/A.py
@@ -1,2 +1,8 @@
-class Pessoa:
-    pass
+class Teste:
+    def __init__(self):
+        self.nome = input('Digite seu nome: ')
+        self.idade = input('Sua idade: ')
+        self.cidade = input('ONde mora? ')
+
+    def jogando(self):
+         print(f'Seu nome é {self.nome}, tem {self.idade} anos e mora em {self.cidade}')
--- cev/B.py
+++ cev/B.py
@@ -1,8 +1,3 @@
-nome = 'CHARLES'
-sobrenome = 'THOMAZ'
-idade = 36
-altura = 1.85
-peso = 90
-ano_nascimento = 2023 - idade
-
-print(f'Este é o {nome} {sobrenome}. Ele nasceu em {ano_nascimento} e tem {idade} anos. {nome} {sobrenome} tem {altura} de altura e pesa {peso}kg. ')
+from A import Teste
+p = Teste()
+print(p)
