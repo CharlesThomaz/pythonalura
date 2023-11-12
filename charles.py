1f017a5a0f6e47d7064a404f958625f27b96c366
CharlesThomaz <charlesthomaz87@gmail.com>
2023-11-11 21:59:07 -0300

    11-11-2023

--- charles.py
+++ charles.py
@@ -1,7 +1,9 @@
-from teste import cria_conta, deposita, saca, extrato
-
-c1 = cria_conta(1,'a',2, 3)
-deposita(c1, 15.2)
-extrato(c1)
-saca(c1, 10.0)
-extrato(c1)
+from conta import Conta
+conta_tassia = Conta(123, 'Tassia',1000.0, 10000.0 )
+print(conta_tassia.extrato())
+conta_tassia.deposita(500.0)
+print(conta_tassia.extrato())
+conta_tassia.deposita(5000.0)
+print(conta_tassia.extrato())
+conta_tassia.saca(6250.0)
+print(conta_tassia.extrato())
--- conta.py
+++ conta.py
@@ -1,12 +1,37 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        print(f'CONSTRUINDO O OBJETO{self}')
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
 
-c1 = Conta(123, 'Charles', 1000.0, 10000.0)
-print(f'Seu saldo é R${c1.saldo}')
-c2 = Conta(456, "Thomaz", 2000.0, 20000.0)
-print(f'Seu saldo é R${c2.saldo}')
+    def extrato(self):
+        print(f'Saldo de R${self.__saldo} do titular {self.__titular}')
+    def deposita(self, valor):
+        self.__saldo += valor
+    def saca(self, valor):
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+    def pega_saldo(self):
+        return self.__saldo
+
+    def devole(self):
+        return self.__saldo
+
+    def pega_saldo(self):
+        return self.__saldo
+
+
+
+
+
+
+
+
+
+
